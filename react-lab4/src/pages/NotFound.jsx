3ce344328a229336fe06132bf1553864b41e4461
Vanshika0312 <chauhanvanshika826@gmail.com>
2026-08-26 22:58:09 +0530

    Add react-lab4

---
+++ react-lab4/src/pages/NotFound.jsx
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom";
+
+function NotFound() {
+
+    return (
+        <div className="page">
+
+            <h1>404</h1>
+
+            <h2>Page Not Found</h2>
+
+            <p>
+                Sorry, the page you are looking for does not exist.
+            </p>
+
+            <Link to="/">
+                Go to Home
+            </Link>
+
+        </div>
+    );
+}
+
+export default NotFound;
